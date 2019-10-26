1d5a85c20d7bc05f479faee8681ff756d5a09c55
Anon_1925 <victor.gso001@gmail.com>
2019-10-25 22:14:41 -0300

    Create admins migration and model

---
+++ database/migrations/2019_10_26_011109_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('username', 255);
+            $table->string('password', 255);
+            $table->string('user', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
