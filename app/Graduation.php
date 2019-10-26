0aa1408ccb5aaa86387910e4447bff360769bce9
Anon_1925 <victor.gso001@gmail.com>
2019-10-25 23:07:21 -0300

    Create params and relations in Graduation model

--- app/Graduation.php
+++ app/Graduation.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Students;
+
 class Graduation extends Model
 {
-    //
+    protected $table = 'graduations';
+
+    public function students() {
+        return $this->belongsToMany(Students::class);
+    }
 }
