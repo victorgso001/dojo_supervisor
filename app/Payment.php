c62a788f262ca4ca5b94432cb5043951e3959b5b
Anon_1925 <victor.gso001@gmail.com>
2019-10-25 23:07:31 -0300

    Create params and relations to Payment model

--- app/Payment.php
+++ app/Payment.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Students;
+
 class Payment extends Model
 {
-    //
+    protected $table = 'payments';
+
+    public function students() {
+        return $this->belongsTo(Students::class);
+    }
 }
