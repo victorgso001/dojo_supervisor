56249e0b589d02c695c6e4e39eda6c0e2de1e121
Anon_1925 <victor.gso001@gmail.com>
2019-10-26 19:25:44 -0300

    Creates admin factory

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Admin;
+use Faker\Generator as Faker;
+
+$factory->define(Admin::class, function (Faker $faker) {
+    return [
+        'username' => $faker->userName,
+        'password' => $faker->password,
+        'user' => $faker->name,
+    ];
+});
