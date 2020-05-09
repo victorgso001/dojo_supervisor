87a548e6bd121601d976c512e996085f1fd54488
Victor <victor.gso001@gmail.com>
2020-05-08 22:43:43 -0300

    [feature/admin-functions] Updates admin Model

--- app/Admin.php
+++ app/Admin.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Jenssegers\Mongodb\Eloquent\Model;
+use \Illuminate\Database\Eloquent\SoftDeletes;
 
 class Admin extends Model
 {
+    use SoftDeletes;
     protected $table = 'admins';
+
+    protected $guarded = ['id'];
 }
